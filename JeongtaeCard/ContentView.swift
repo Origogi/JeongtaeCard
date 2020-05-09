6f7a1902ef23db1e9c9c0a323fe29200b349ea66
Jeongtae Kim <rlawjdxo1212@naver.com>
2020-05-09 19:46:55 +0900

    Initial Commit

---
+++ JeongtaeCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JeongtaeCard
+//
+//  Created by 김정태 on 2020/05/09.
+//  Copyright © 2020 김정태. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
